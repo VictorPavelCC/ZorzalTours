9b9ce73e3e17db24a0eeaf6a1c8b8825551efe0f
Victor Pavel Cuentas Calle <pavelcuentas@gmail.com>
2022-10-24 00:49:26 -0300

    cambiando clases

--- js/productos.js
+++ js/productos.js
@@ -12,5 +12,47 @@ const productos = [
         precio: 15000,
         img:"../img/cordoba1.jpg"
     
-    }
+    },
+    {
+        id: 3,
+        nombre: "Misiones",
+        precio: 10000,
+        img:"../img/cataratasbote.webp"
+    
+    },
+    {
+        id: 4,
+        nombre: "Misiones",
+        precio: 10000,
+        img:"../img/cataratasbote.webp"
+    
+    },
+    {
+        id: 5,
+        nombre: "Misiones",
+        precio: 10000,
+        img:"../img/cataratasbote.webp"
+    
+    },
+    {
+        id: 6,
+        nombre: "Misiones",
+        precio: 10000,
+        img:"../img/cataratasbote.webp"
+    
+    },
+    {
+        id: 7,
+        nombre: "Misiones",
+        precio: 10000,
+        img:"../img/cataratasbote.webp"
+    
+    },
+    {
+        id: 8,
+        nombre: "Misiones",
+        precio: 10000,
+        img:"../img/cataratasbote.webp"
+    
+    },
 ];
